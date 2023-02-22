dbf072acbc21e015b2dddce83fc02113da9b6d30
hl.lei <1204062841@qq.com>
2023-02-22 20:57:58 +0800

    Initial commit

---
+++ spark-01/src/main/java/com/lhl/Test.scala
@@ -0,0 +1,10 @@
+package com.lhl
+
+object Test {
+
+    def main(args: Array[String]): Unit = {
+
+        println("Hello Spark")
+//E:\HDU\Java\Resources\Spark\3.代码\spark-core
+    }
+}
---
+++ spark-01/src/main/java/com/lhl/core/framework/common/TService.scala
@@ -0,0 +1,5 @@
+package com.lhl.core.framework.common
+
+trait TService {
+    def dataAnalysis():Any
+}
